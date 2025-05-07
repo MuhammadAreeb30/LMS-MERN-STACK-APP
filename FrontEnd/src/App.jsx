b9a3f4828e08cd7aef9f1622687a4f3d896c1db7
MuhammadAreeb30 <almostevil55@gmail.com>
2025-05-07 16:43:23 +0500

    FrontEnd Setup

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+   <>
+    <Button className="bg-fuchsia-600 cursor-pointer">Hello World</Button>
+   </>
+  )
+}
+
+export default App
